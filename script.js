43526c5fe13ad2b4cb8969efeb20f09e815a8db2
Ariel Santos <aricvs@gmail.com>
2023-10-20 20:50:39 -0300

    Set return values for the conditionals in playRound

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function getComputerChoice () {
+function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3 + 1)
     return computerChoice
 }
 
+function playRound(playerSelection, computerSelection) {
+    const drawResult = `Draw!`;
+    const lossResult = `You lose! ${computerSelection} beats ${playerSelection}`;
+    const winResult = `You win! ${playerSelection} beats ${computerSelection}`;
+
+    if (playerSelection === computerSelection ) {
+        return drawResult
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return lossResult
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return winResult
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return lossResult
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return winResult
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return lossResult
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return winResult
+    }
+}
+
+console.log(playRound('rock', getComputerChoice()))

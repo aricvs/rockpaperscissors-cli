bc5039f352c04820bdbff546b31aa77241c27344
Ariel Santos <aricvs@gmail.com>
2023-10-20 20:53:27 -0300

    Fix getComputerChoice function to return string

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3 + 1)
+    const randomNumber = Math.floor(Math.random() * 3 + 1)
+    let computerChoice
+
+    switch (randomNumber) {
+        case 1:
+            computerChoice = 'rock';
+            break;
+        case 2:
+            computerChoice = 'paper';
+            break;
+        case 3:
+            computerChoice = 'scissors';
+            break;
+    }
+
     return computerChoice
 }
 

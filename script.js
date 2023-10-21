a554b816e7ab3bdcecc92a0bd61c4b6c08aa6fd3
Ariel Santos <aricvs@gmail.com>
2023-10-20 21:04:34 -0300

    Format code using Prettier

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3 + 1)
-    let computerChoice
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerChoice;
 
-    switch (randomNumber) {
-        case 1:
-            computerChoice = 'rock';
-            break;
-        case 2:
-            computerChoice = 'paper';
-            break;
-        case 3:
-            computerChoice = 'scissors';
-            break;
-    }
+  switch (randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+  }
 
-    return computerChoice
+  return computerChoice;
 }
-
 
 function getPlayerChoice() {
-    let playerChoice
+  let playerChoice;
 
-    while (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
-        playerChoice = prompt("Make your play: rock, paper or scissors?");
-        playerChoice = playerChoice.toLowerCase();
-    }
-    
-    return playerChoice
+  while (
+    playerChoice !== "rock" &&
+    playerChoice !== "paper" &&
+    playerChoice !== "scissors"
+  ) {
+    playerChoice = prompt("Make your play: rock, paper or scissors?");
+    playerChoice = playerChoice.toLowerCase();
+  }
+
+  return playerChoice;
 }
-
 
 function playRound(playerSelection, computerSelection) {
-    const drawResult = `Draw!`;
-    const lossResult = `You lose, ${computerSelection} beats ${playerSelection}!`;
-    const winResult = `You win, ${playerSelection} beats ${computerSelection}!`;
+  const drawResult = `Draw!`;
+  const lossResult = `You lose, ${computerSelection} beats ${playerSelection}!`;
+  const winResult = `You win, ${playerSelection} beats ${computerSelection}!`;
 
-    if (playerSelection === computerSelection ) {
-        return drawResult
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return lossResult
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return winResult
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return lossResult
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return winResult
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return lossResult
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return winResult
-    }
+  if (playerSelection === computerSelection) {
+    return drawResult;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return lossResult;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return winResult;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return lossResult;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return winResult;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return lossResult;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return winResult;
+  }
 }
-
-getPlayerChoice()
